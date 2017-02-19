2f168ff05409bbda3c70008a164c75195ff4268b
Masaaki Hirotsu <hirotsu.masaaki@gmail.com>
2017-02-20 00:06:16 +0900

    fix incorrect url

--- script/install_whalebrew.sh
+++ script/install_whalebrew.sh
@@ -2,5 +2,5 @@
 set -eu
 
 WHALEBREW_VERSION=0.0.4
-sudo curl -L "https://github.com/bfirsh/whalebrew/releases/download/0.0.4/${WHALEBREW_VERSION}-$(uname -s)-$(uname -m)" -o /usr/local/bin/whalebrew
+sudo curl -L "https://github.com/bfirsh/whalebrew/releases/download/${WHALEBREW_VERSION}/whalebrew-$(uname -s)-$(uname -m)" -o /usr/local/bin/whalebrew
 sudo chmod +x /usr/local/bin/whalebrew
