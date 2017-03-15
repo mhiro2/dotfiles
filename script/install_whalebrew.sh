bb652ec7ce62c81ac47248ab026b67bc11c0f75b
mhrio2 <hirotsu.masaaki@gmail.com>
2017-03-15 23:55:16 +0900

    update whalebrew version

--- script/install_whalebrew.sh
+++ script/install_whalebrew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-WHALEBREW_VERSION=0.0.4
+WHALEBREW_VERSION=0.0.5
 sudo curl -L "https://github.com/bfirsh/whalebrew/releases/download/${WHALEBREW_VERSION}/whalebrew-$(uname -s)-$(uname -m)" -o /usr/local/bin/whalebrew
 sudo chmod +x /usr/local/bin/whalebrew
