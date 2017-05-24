ee777679dc0c7aba627a2562308d822939802bc4
mhrio2 <hirotsu.masaaki@gmail.com>
2017-05-24 21:21:46 +0900

    add kompose installation script

---
+++ script/install_kompose.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -eu
+
+KOMPOSE_VERSION="v0.6.0"
+curl -L https://github.com/kubernetes-incubator/kompose/releases/download/${KOMPOSE_VERSION}/kompose-linux-amd64 -o /usr/local/bin/kompose
+sudo chmod +x /usr/local/bin/whalebrew
