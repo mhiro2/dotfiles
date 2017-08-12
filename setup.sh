ffe59f521f5e75fbe78d852569f9841311ae775e
mhrio2 <hirotsu.masaaki@gmail.com>
2017-08-12 19:51:52 +0900

    modify path definition with xdg_config_home

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ git submodule init
 git submodule update
 
 zplug_install () {
-  #local installer='https://zplug.sh/installer'
   local installer='https://raw.githubusercontent.com/zplug/installer/master/installer.zsh'
   curl -sL --proto-redir -all,https $installer | zsh
 }
@@ -27,19 +26,30 @@ do
   esac
 done
 
-mkdir -p ${HOME}/.config
+mkdir -p ${XDG_CONFIG_HOME}
 
 # fish
-if [ ! -L "${HOME}/.config/fish" ]; then
-  ln -sf "${PWD}/fish" "${HOME}/.config/fish"
+if [ ! -L "${XDG_CONFIG_HOME}/fish" ]; then
+  ln -sf "${PWD}/fish" "${XDG_CONFIG_HOME}/fish"
 fi
 
 # neovim
-if [ ! -L "${HOME}/.config/nvim" ]; then
-  ln -sf "${PWD}/nvim" "${HOME}/.config/nvim"
+if [ ! -L "${XDG_CONFIG_HOME}/nvim" ]; then
+  ln -sf "${PWD}/nvim" "${XDG_CONFIG_HOME}/nvim"
+fi
+
+# sway
+if [ ! -L "${XDG_CONFIG_HOME}/sway" ]; then
+  ln -sf "${PWD}/sway" "${XDG_CONFIG_HOME}/sway"
+fi
+
+# terminator
+if [ ! -L "${XDG_CONFIG_HOME}/terminator" ]; then
+  ln -sf "${PWD}/terminator" "${XDG_CONFIG_HOME}/terminator"
 fi
 
 # yapf
-if [ ! -L "${HOME}/.config/yapf" ]; then
-  ln -sf "${PWD}/yapf" "${HOME}/.config/yapf"
+if [ ! -L "${XDG_CONFIG_HOME}/yapf" ]; then
+  ln -sf "${PWD}/yapf" "${XDG_CONFIG_HOME}/yapf"
 fi
+
