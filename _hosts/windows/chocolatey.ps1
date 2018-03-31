c8465e027c394e2c7090948149da669828c3c79d
mhrio2 <hirotsu.masaaki@gmail.com>
2018-03-31 18:05:46 +0900

    :+1: update chocolatey packages

--- _hosts/windows/chocolatey.ps1
+++ _hosts/windows/chocolatey.ps1
@@ -2,36 +2,53 @@
 # chocolatey packages
 #----------------------------
 
-## System Utilities
-cinst autohotkey.portable
-cinst ccleaner
-cinst procexp
-
 ## Applications
-cinst aimp
-cinst conemu
-cinst etcher
-cinst firefox
-cinst foxitreader
-cinst googlechrome
-cinst googlejapaneseinput
-cinst hain
-cinst skype
-cinst tablacus
-cinst vlc
+cinst -y aimp
+cinst -y autohotkey
+cinst -y autohotkey.portable
+cinst -y autoruns
+cinst -y ccleaner
+cinst -y conemu
+cinst -y ctrl2cap
+cinst -y etcher
+cinst -y firefox
+cinst -y foxitreader
+cinst -y googlechrome
+cinst -y googlejapaneseinput
+cinst -y hain
+cinst -y honeyview
+cinst -y kindle
+#cinst -y mailspring
+#cinst -y odrive
+cinst -y procexp
+cinst -y rufus
+cinst -y slack
+#cinst -y tablacus
+cinst -y vlc
 
 ## Developer tools
-cinst ag
-cinst awscli
-cinst curl
-cinst docker
-cinst git.install
-cinst golang
-cinst nodejs.install
-cinst python2
-cinst ripgrep
-cinst vagrant
-cinst virtualbox
-cinst visualstudiocode
-cinst wget
-cinst which
+cinst -y ag
+cinst -y awscli
+cinst -y curl
+cinst -y docker-compose
+cinst -y docker-for-windows
+cinst -y ffmpeg
+cinst -y git.install
+cinst -y golang
+cinst -y imagemagick
+cinst -y jq
+cinst -y kubernetes-cli
+cinst -y miniconda3
+cinst -y neovim
+cinst -y nmap
+cinst -y openssh
+cinst -y pandoc
+cinst -y peco
+cinst -y ripgrep
+#cinst -y vagrant
+#cinst -y virtualbox
+cinst -y visualstudiocode
+cinst -y unzip
+cinst -y wget
+cinst -y which
+
