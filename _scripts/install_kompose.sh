7afe13c612c9b1a625149dd814d0f13a863b26e5
mhrio2 <hirotsu.masaaki@gmail.com>
2018-04-22 16:18:58 +0900

    :shower: remove unused scripts

--- _scripts/install_kompose.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-KOMPOSE_VERSION="v1.4.0"
-sudo curl -L https://github.com/kubernetes-incubator/kompose/releases/download/${KOMPOSE_VERSION}/kompose-linux-amd64 -o /usr/local/bin/kompose
-sudo chmod +x /usr/local/bin/kompose
--- _scripts/install_linuxbrew.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-command -v ruby > /dev/null && \
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
