87f6083e98285b27665a5e29746ee27da3fc3771
mhrio2 <hirotsu.masaaki@gmail.com>
2018-04-25 23:34:13 +0900

    :recycle: refactor and cleanup zsh plugins

--- .zsh/zplug.zsh
+++ .zsh/zplug.zsh
@@ -1,93 +1,84 @@
 # local loading
-zplug "zplug/zplug"
+zplug 'zplug/zplug', hook-build:'zplug --self-manage'
 
 ##------------------------------
 # zsh plugins
 
 # git prompt
-zplug "lib/git", from:oh-my-zsh
+zplug 'lib/git', from:oh-my-zsh
 
 # Syntax highlighting.
-zplug "zsh-users/zsh-syntax-highlighting", defer:2
+zplug 'zsh-users/zsh-syntax-highlighting', defer:2
 
 # History search.
-zplug "zsh-users/zsh-history-substring-search"
+zplug 'zsh-users/zsh-history-substring-search'
 
 # Completions.
-zplug "zsh-users/zsh-completions"
-zplug "plugins/docker", from:oh-my-zsh
-zplug "plugins/docker-compose", from:oh-my-zsh
-zplug "plugins/kubectl", from:oh-my-zsh
+zplug 'zsh-users/zsh-completions', use:'src/_*', lazy:true
 
 # Visual mode for vi-mode.
-zplug "b4b4r07/zsh-vimode-visual", defer:3
+zplug 'b4b4r07/zsh-vimode-visual', defer:3
 
 ##------------------------------
 # Useful commands
 
 # anyframe
-zplug "mollifier/anyframe", \
-    at:4c23cb60
+zplug 'mollifier/anyframe'
 
-# # ctop
-# zplug "bcicen/ctop", \
-#     as:command, \
-#     from:gh-r, \
-#     rename-to:"ctop"
+# checksec.sh
+zplug 'slimm609/checksec.sh', \
+  as:command, \
+  use:'checksec'
 
-# # enhancd
-# zplug "b4b4r07/enhancd", \
-#     use:init.sh
-
-# # exa
-# zplug "ogham/exa", \
-#     as:command, \
-#     rename-to:"exa", \
-#     from:gh-r
-
-# # fzf
-#zplug "junegunn/fzf-bin", \
-#    as:command, \
-#    from:gh-r, \
-#    rename-to:"fzf", \
-#    frozen:1
+# fzf
+zplug 'junegunn/fzf-bin', \
+  as:command, \
+  from:gh-r, \
+  rename-to:fzf
 
 # ghq
-zplug "motemen/ghq", \
-    as:command, \
-    from:gh-r
+zplug 'motemen/ghq', \
+  as:command, \
+  from:gh-r
 
 # jq
-zplug "stedolan/jq", \
-    as:command, \
-    from:gh-r, \
-    frozen:1
+zplug 'stedolan/jq', \
+  as:command, \
+  from:gh-r
+
+# lstf
+zplug 'yuuki/lstf', \
+  as:command, \
+  from:gh-r
 
 # peco
-zplug "peco/peco", \
-    as:command, \
-    from:gh-r, \
-    frozen:1
+zplug 'peco/peco', \
+  as:command, \
+  from:gh-r
 
 # pget
-zplug "Code-Hex/pget", \
-    as:command, \
-    from:gh-r, \
-    frozen:1
+zplug 'Code-Hex/pget', \
+  as:command, \
+  from:gh-r
 
 # pt
-zplug "monochromegane/the_platinum_searcher", \
-    as:command, \
-    from:gh-r, \
-    rename-to:"pt", \
-    frozen:1
+zplug 'monochromegane/the_platinum_searcher', \
+  as:command, \
+  from:gh-r, \
+  rename-to:pt
+
+# ripgrep
+zplug 'BurntSushi/ripgrep', \
+  as:command, \
+  from:gh-r, \
+  rename-to:rg
 
 # tmux-xpanes
-zplug "greymd/tmux-xpanes"
+zplug 'greymd/tmux-xpanes'
 
 ##------------------------------
 # other
 
 # gdb-peda
-zplug "longld/peda"
+zplug 'longld/peda'
 
