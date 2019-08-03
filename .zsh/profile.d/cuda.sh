a17533a2c66acb83340fefef226ce33e6ea571bc
mhrio2 <hirotsu.masaaki@gmail.com>
2019-08-03 16:49:47 +0900

    :shower: remove unused profile in zsh

--- .zsh/profile.d/cuda.sh
+++
@@ -1,7 +0,0 @@
-export CUDA_ROOT="/usr/local/cuda"
-if [ -d "${CUDA_ROOT}" ]; then
-  export PATH=${CUDA_ROOT}/bin:$PATH
-  export CPATH=${CUDA_ROOT}/include:$CPATH
-  export LD_LIBRARY_PATH=${CUDA_ROOT}/lib64:$LD_LIBRARY_PATH
-  export LIBRARY_PATH=${CUDA_ROOT}/lib64:$LIBRARY_PATH
-fi
