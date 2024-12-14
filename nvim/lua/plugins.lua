b1bb7e0a040f9a5a689cdc4865adf2bf50a0d066
Masaaki Hirotsu <hirotsu.masaaki@gmail.com>
2024-12-14 18:02:06 +0900

    :sparkles: add noice.nvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -113,6 +113,32 @@ return {
     end,
   },
   {
+    "folke/noice.nvim",
+    opts = {},
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+      })
+    end,
+  },
+  {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
     config = true,
