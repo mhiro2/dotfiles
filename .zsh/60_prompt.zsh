c1cdb95685556f6abafde4c2dd66580e51561a69
Masaaki Hirotsu <hirotsu.masaaki@gmail.com>
2025-07-12 15:39:27 +0900

    :wrench: update prompt colors for better readability

--- .zsh/60_prompt.zsh
+++ .zsh/60_prompt.zsh
@@ -1,10 +1,10 @@
-HOST_PROMPT="%K{160}%F{255}  %F{160}%k%F{214} %n@%m%f%k%f:"
-DIR_PROMPT="%F{87}%~ %k%f"
+HOST_PROMPT="%K{124}%F{255}  %F{88}%k%F{214} %n@%m%f%k%f:"
+DIR_PROMPT="%F{81}%~ %k%f"
 PROMPT_HEAD="%F{1}%F{3}%F{2}%f"
 
 ZSH_THEME_GIT_SHOW_UPSTREAM=1
-ZSH_THEME_GIT_PROMPT_PREFIX="%K{2}%F{0}  "
-ZSH_THEME_GIT_PROMPT_SUFFIX=" %K{0}%F{2}%k%f"
+ZSH_THEME_GIT_PROMPT_PREFIX="%K{34}%F{0}  "
+ZSH_THEME_GIT_PROMPT_SUFFIX=" %K{0}%F{34}%k%f"
 
 # Remove unnecessary quotation marks based on the following:
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/git.zsh
