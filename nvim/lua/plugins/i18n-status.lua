0fdc54bd168c0a4f190018daa844d3e448b066e6
Masaaki Hirotsu <hirotsu.masaaki@gmail.com>
2026-04-29 13:39:15 +0900

    fix(nvim): move i18n-status keymaps out of setup options

--- nvim/lua/plugins/i18n-status.lua
+++ nvim/lua/plugins/i18n-status.lua
@@ -7,11 +7,11 @@ return {
       inline = {
         position = "after_key",
       },
-
-      vim.keymap.set("n", "<Space>in", "<Cmd>I18nLangNext<CR>", { desc = "Next language" }),
-      vim.keymap.set("n", "<Space>ip", "<Cmd>I18nLangPrev<CR>", { desc = "Previous language" }),
-      vim.keymap.set("n", "<Space>id", "<Cmd>I18nDoctor<CR>", { desc = "i18n doctor" }),
-      vim.keymap.set("n", "<Space>ia", "<Cmd>I18nAddKey<CR>", { desc = "Add new i18n key" }),
     })
+
+    vim.keymap.set("n", "<Space>in", "<Cmd>I18nLangNext<CR>", { desc = "Next language" })
+    vim.keymap.set("n", "<Space>ip", "<Cmd>I18nLangPrev<CR>", { desc = "Previous language" })
+    vim.keymap.set("n", "<Space>id", "<Cmd>I18nDoctor<CR>", { desc = "i18n doctor" })
+    vim.keymap.set("n", "<Space>ia", "<Cmd>I18nAddKey<CR>", { desc = "Add new i18n key" })
   end,
 }
