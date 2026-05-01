e2d6b9d650d782920fbac93f5320a7a30cb758f7
Masaaki Hirotsu <hirotsu.masaaki@gmail.com>
2026-05-01 16:24:35 +0900

    refactor(nvim): use vim.system for lazy.nvim bootstrap

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,14 +8,14 @@ vim.g.maplocalleader = ","
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.uv.fs_stat(lazypath) then
-  vim.fn.system({
+  vim.system({
     "git",
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
     "--branch=stable",
     lazypath,
-  })
+  }):wait()
 end
 vim.opt.rtp:prepend(lazypath)
 
